ca86f47bb5f9d5788d70fb88dd02f5f65b3c58c5
Hailee <hjwheat@cpp.edu>
2023-05-14 23:10:40 -0700

    Added MySQL file!

---
+++ Customers_table.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM payments.customers
+LIMIT 0, 1000
+
+-- Date: 2023-05-14 23:06
+*/
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Hailee',47810.00,500.00,20.00,'600-659','48',1212.10,58680.86);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Hauilee',68762.00,5000.00,220.00,'720-850','48',1521.06,78011.01);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Jaz',29524.00,909.00,123.00,'600-659','60',603.54,37121.46);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Youp',76571.00,7000.00,500.00,'720-850','72',1134.52,88685.53);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Wei',37524.00,2000.00,100.00,'300-599','36',1226.42,46151.10);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Bob',5143.00,10.00,5.00,'300-599','72',100.71,7261.48);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Down Payment',80000.00,0.00,200.00,'300-599','36',2752.70,99097.18);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Down Payment',80000.00,2666.00,200.00,'300-599','36',2666.67,98666.30);
+INSERT INTO `` (`Name`,`Price`,`Down_Payment`,`Trade_In_Value`,`Credit_Score`,`Loan_Term`,`Monthly_Payment`,`Total`) VALUES ('Diana',58476.00,5000.00,500.00,'720-850','72',869.87,67630.89);
